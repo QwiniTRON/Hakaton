7e3d9bb0e51495b05c815b6fd8d1a2cfb8ec8f44
Arcusterrr <nikita.kargapolov000@gmail.com>
2019-11-24 02:26:02 +0500

    5 hours

--- src/core/api/index.ts
+++ src/core/api/index.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { PORT, SERVER_IP } from "./server-config";
+import { as } from "pg-promise";
+import { TIMEOUT } from "dns";
 
 export async function login(login: string, pass: string) {
   return await axios.post(`http://${SERVER_IP}:${PORT}/login`, {
@@ -8,6 +10,107 @@ export async function login(login: string, pass: string) {
   });
 }
 
-login("Yaniddze", "123123").then(res1 => {
+export async function register(
+  login: string,
+  pass: string,
+  name: string,
+  cou: string,
+  cit: string,
+  str: string,
+  house: string,
+  indd: string
+) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/register`, {
+    log: login,
+    pas: pass,
+    nam: name,
+    cu: cou,
+    ct: cit,
+    st: str,
+    hou: house,
+    ind: indd
+  });
+}
+
+export async function getuserbytoken(token: string) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/login`, {
+    tok: token
+  });
+}
+
+export async function usercheck(login: string) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/usercheck`, {
+    log: login
+  });
+}
+
+export async function add_category(tok: string, tii: string, mcic: number) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/addcategory`, {
+    token: tok,
+    tit: tii,
+    mci: mcic
+  });
+}
+
+export async function add_main_category(tok: string, tii: string) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/addmaincategory`, {
+    token: tok,
+    tit: tii
+  });
+}
+
+export async function add_product(
+  tok: string,
+  abou: string,
+  tii: string,
+  pr: number
+) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/addproduct`, {
+    token: tok,
+    abo: abou,
+    tit: tii,
+    pri: pr
+  });
+}
+
+export async function add_sale(tok: string, tii: string, pr: number) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/addsale`, {
+    token: tok,
+    tit: tii,
+    per: pr
+  });
+}
+
+export async function addcattoproduct(token: string, cid: number, pid: number) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/addcategorytoproduct`, {
+    tok: token,
+    c_id: cid,
+    p_id: pid
+  });
+}
+
+export async function addtowish(token: string, pid: number) {
+  return await axios.post(`http://${SERVER_IP}:${PORT}/addtowish`, {
+    tok: token,
+    p_id: pid
+  });
+}
+
+// add_main_category("20b3c1336564e9ad4ce321005eee809a", "Бас гитары").then(
+//   res1 => {
+//     console.log(res1.data);
+//   }
+// );
+
+// add_product(
+//   "20b3c1336564e9ad4ce321005eee809a",
+//   "Легенда среди акустических гитар",
+//   "Gibson L-48 (1963)",
+//   50000
+// ).then(res1 => {
+//   console.log(res1.data);
+// });
+
+addcattoproduct("20b3c1336564e9ad4ce321005eee809a", 2, 2).then(res1 => {
   console.log(res1.data);
 });
