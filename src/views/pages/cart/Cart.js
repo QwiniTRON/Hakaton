07c0348905569233f1d86782e431680036adf59b
QwiniTRON <barabanzz871@gmail.com>
2019-11-25 19:37:57 +0500

    cc

--- src/views/pages/cart/Cart.js
+++ src/views/pages/cart/Cart.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Header from "../../layouts/header/Header";
 import Footer from "../../layouts/footer/Footer";
 import Itemfull from "../../components/itemFull/Itemfull";
+import "./cart.css";
 
 export default class Cart extends React.Component{
     constructor(props){
